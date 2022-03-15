6c050e695c5362012692ea9044f97abd148ec917
Code Sandbox <hello@codesandbox.io>
2022-03-15 06:39:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dd8bc256394dc304254994661c98b12c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6e437aa97d37a9b98c14e45c149749",
+    "url": "https://swarnim55.github.io/csb-idnoiy/index.html"
+  },
+  {
+    "revision": "8289e9909a02313c7c7c",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/css/2.c3097103.chunk.css"
+  },
+  {
+    "revision": "d290add3f4e995ab620f",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/css/main.86c3eb02.chunk.css"
+  },
+  {
+    "revision": "8289e9909a02313c7c7c",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/js/2.db1f3661.chunk.js"
+  },
+  {
+    "revision": "28b5e57e1e89ae408eee8d8f4d692c7c",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/js/2.db1f3661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d290add3f4e995ab620f",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/js/main.2b10ff1b.chunk.js"
+  },
+  {
+    "revision": "4366149eab5460887476",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/js/runtime-main.5cf8aea7.js"
+  },
+  {
+    "revision": "e8592292b074b7778e064a1c78185969",
+    "url": "https://swarnim55.github.io/csb-idnoiy/static/media/logo.e8592292.jpg"
+  }
+]);
